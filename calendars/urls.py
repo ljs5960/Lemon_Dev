4675857975cad31be8c6b07a6b0f19992bff2d49
ljs5960 <koreastars1@naver.com>
2022-01-24 14:59:20 +0900

    solving merge conflicts

--- calendars/urls.py
+++ calendars/urls.py
@@ -11,22 +11,11 @@ app_name = 'calendars'
 urlpatterns = [
 
     path('', views.home, name='home'),
-<<<<<<< HEAD
-    path('calendar', views.calendar, name='calendar'),
-    path('summary', views.summary, name='summary'), # 요약페이지
-    path('recom', views.recom, name='recom'), # 추천페이지
-    path('list_view', views.list_view, name='list_view'), # 내역페이지
-    #path('calendar', views.list, name='list'),
-    # path('cal_list', views.cal_list, name='calendarList'),
-
-    # path('addlist', views.addlist, name='addlist'),
-=======
     path('top5', views.top5, name='top5'),
     path('summary', views.summary, name='summary'), # 요약페이 지
     path('recom', views.recom, name='recom'), # 추천페이지
     path('listview', views.listview, name='listview'), # 내역페이지
     path('detail_search', views.detail_search, name='detail_search'), # 내역필터페이지
->>>>>>> ee28057a88abcddca667acd6012343dd44b687ee
 
     path('add_calendar/', views.add_calendar, name='add_calendar'),
     path('edit_calendar/<str:kind>/<int:spend_id>/', views.edit_calendar, name='edit_calendar'),
