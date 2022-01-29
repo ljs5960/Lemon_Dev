fbf43a34fe81bee833ab0ffa2c3325f94adc2e41
ljs5960 <koreastars1@naver.com>
2022-01-29 20:51:55 +0900

    testing aws prod

--- lemon/settings/base.py
+++ lemon/settings/base.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 INSTALLED_APPS = [
--- lemon/wsgi.py
+++ lemon/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lemon.settings.prod')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lemon.settings.base')
 
 application = get_wsgi_application()
