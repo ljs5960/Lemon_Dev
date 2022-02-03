db53475664ada233be0e67e2d58f7719bbdff25a
ljs5960 <koreastars1@naver.com>
2022-02-03 17:42:51 +0900

    edit Allowed Hosts

--- lemon/settings/prod.py
+++ lemon/settings/prod.py
@@ -2,4 +2,9 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['Lemon-dev.ap-northeast-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = [
+  'Lemon-dev.ap-northeast-2.elasticbeanstalk.com',
+  '52.78.138.222',
+  'lemon-bs.com',
+  'www.lemon-bs.com',
+  ]
