deba49acffefb0b0838e786403e5d55b3e71d7d9
ljs5960 <koreastars1@naver.com>
2022-02-07 14:47:47 +0900

    Edit 'myinfo' location

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,7 +10,7 @@ app_name='accounts'
 
 urlpatterns = [
     path('main', views.main, name='main'),
-    path('myinfo', views.myinfo, name='myinfo'),
+    # path('myinfo', views.myinfo, name='myinfo'),
     path('signup', views.signup, name='signup'),
     path('login', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
